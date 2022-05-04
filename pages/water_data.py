e25974b2ca3e1aa3781a1cc0b39a4993ccbdd8ba
LisaSaraMara <104788047+LisaSaraMara@users.noreply.github.com>
2022-05-04 19:36:33 +0200

    Update water_data.py

--- pages/water_data.py
+++ pages/water_data.py
@@ -32,60 +32,7 @@ from streamlit_folium import folium_static
 # import warnings
 # warnings.filterwarnings("ignore")
 
-# df_sample = pd.read_csv('data/Drinking_Water_Quality_Distribution_Monitoring_Data.csv')
-# df_sites = pd.read_excel('data/OpenData_Distribution_Water_Quality_Sampling_Sites_Updated_2021-0618.xlsx')
-# df = pd.merge(df_sample, df_sites, on='Sample Site')
-
-
-# source_crs = 'epsg:2263' # Coordinate system of the file from https://aem.run/posts/2021-07-02-learning-the-basics-of-gis-mapping-with-leaflet/
-# target_crs = 'epsg:4326' # Global lat-lon coordinate system
-
-# polar_to_latlon = pyproj.Transformer.from_crs(source_crs, target_crs)
-# df['lat'], df['lon'] = polar_to_latlon.transform(df['X - Coordinate'], df['Y - Coordinate'])
-
-# df.drop(columns=['X - Coordinate', 'Y - Coordinate'], inplace=True)
-
-# df['Fluoride (mg/L)'].replace({'<0.3':'0.3'}, inplace=True)
-# df['Fluoride (mg/L)'] = df['Fluoride (mg/L)'].astype('float')
-
-# df['Turbidity (NTU)'].replace({'<0.10':'0.1'}, inplace=True)
-# df['Turbidity (NTU)'] = df['Turbidity (NTU)'].astype('float')
-
-# df['Coliform_float'] = df['Coliform (Quanti-Tray) (MPN /100mL)'].replace({'<1':'0', '>200.5':'201'})
-# df['Coliform_float'] = df['Coliform_float'].astype('float')
-
-# df['Ecoli_float'] = df['E.coli(Quanti-Tray) (MPN/100mL)'].replace({'<1':'0', '>200.5':'201'})
-# df['Ecoli_float'] = df['Ecoli_float'].astype('float')
-
-# df['Year'] = df['Sample Date'].str.split('/').str[2]
-# df['Month'] = df['Sample Date'].str.split('/').str[0]
-# df['Day'] = df['Sample Date'].str.split('/').str[1]
-
-df = pd.read_csv('data/water_data_preprocessed.csv')
-
-# apparently, there was a type-error
-df['Sample Site'] = df['Sample Site'].astype(str)
-
-### Borough allocation
-
-for index, row in df.iterrows():
-    if row['Sample Site'].startswith('2'):
-        df.at[index, 'color'] = 'yellow'
-        df.at[index, 'borough'] = 'Brooklyn'
-    elif row['Sample Site'].startswith('5'):
-        df.at[index, 'color'] = 'purple'
-        df.at[index, 'borough'] = 'Staten Island'
-    elif row['Sample Site'].startswith('4') or row['Sample Site'].startswith('7'):
-        df.at[index, 'color'] = 'orange'
-        df.at[index, 'borough'] = 'Queens'
-    elif row['Sample Site'].startswith('1'):
-        df.at[index, 'color'] = 'red'
-        df.at[index, 'borough'] = 'Bronx'
-    elif row['Sample Site'].startswith('3'):
-        df.at[index, 'color'] = 'green'
-        df.at[index, 'borough'] = 'Manhattan'
-    else: 
-        df.at[index, 'color'] = 'blue'
+df = pd.read_csv('data/Water_quality.csv')
 
 df_loc = df[['Sample Site', 'lat', 'lon', 'color', 'borough']]
 df_loc = df_loc.drop_duplicates().reset_index()
