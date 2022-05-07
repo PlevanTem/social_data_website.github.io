7097a60b35c2c3cd00c29c36e64f68e6b75f2bde
LisaSaraMara <104788047+LisaSaraMara@users.noreply.github.com>
2022-05-07 19:24:26 +0200

    Update water_data.py

--- pages/water_data.py
+++ pages/water_data.py
@@ -51,10 +51,97 @@ for index,row in df_loc.iterrows():
     folium.CircleMarker([row['lat'], row['lon']], popup=f'Sample Site: {pop}, Borough: {bor}', color=row['color'],
             fill=True, opacity=0.5, radius = 2).add_to(ny_map)
 
-# Plot the development of the water quality over time for each sample site
+# Prepare year and month column
 df['Year - Month'] = df['Year'].astype('str') + "-" + df['Month'].astype('str')
+# sorted Year Month
+sorted_year_month = ['2015-1', '2015-2', '2015-3', '2015-4', '2015-5', '2015-6', '2015-7', '2015-8', '2015-9', '2015-10', 
+          '2015-11', '2015-12', '2016-1', '2016-2', '2016-3', '2016-4', '2016-5', '2016-6', '2016-7', '2016-8', 
+          '2016-9', '2016-10', '2016-11', '2016-12', '2017-1', '2017-2', '2017-3', '2017-4', '2017-5', '2017-6', 
+          '2017-7', '2017-8', '2017-9', '2017-10', '2017-11', '2017-12', '2018-1', '2018-2', '2018-3','2018-4', 
+          '2018-5', '2018-6', '2018-7', '2018-8', '2018-9', '2018-10', '2018-11', '2018-12', '2019-1', '2019-2', 
+          '2019-3', '2019-4', '2019-5', '2019-6', '2019-7', '2019-8', '2019-9', '2019-10', '2019-11', '2019-12',
+          '2020-1', '2020-2', '2020-3', '2020-4', '2020-5', '2020-6', '2020-7', '2020-8', '2020-9', '2020-10', 
+          '2020-11', '2020-12', '2021-1', '2021-2', '2021-3', '2021-4','2021-5', '2021-6', '2021-7', '2021-8', 
+          '2021-9','2021-10', '2021-11','2021-12', '2022-1', '2022-2', '2022-3']
+
+
+### Bokeh plot of the number of samples for different time periods
+
+### prep the data 
+df_no_samples_month_year  = df.groupby('Year - Month').agg({'Sample Number':'count'}).reset_index()
+df_no_samples_day = df.groupby('Day').agg({'Sample Number':'count'}).reset_index()
+df_no_samples_month = df.groupby('Month').agg({'Sample Number':'count'}).reset_index()
+df_no_samples_year = df.groupby('Year').agg({'Sample Number':'count'}).reset_index()
+
+cds_month_year = ColumnDataSource(df_no_samples_month_year)
+cds_day = ColumnDataSource(df_no_samples_day)
+cds_month = ColumnDataSource(df_no_samples_month)
+cds_year = ColumnDataSource(df_no_samples_year)
+
+list_of_bars = ['Sample Number']
+colors = ['blue']
+dict_legend = {'Sample Number':'No of samples'}
+
+# Create a general function for customizing legends
+def customize_legend(p, items): 
+    p.legend.visible = False
+    legend = Legend(items=items, location="bottom_center")
+    p.add_layout(legend, 'below')
+    p.legend.click_policy = "mute"
+
+# Create the general layout of the 3 plots with width, height, title and axes labels
+p11 = figure(plot_width = 900, plot_height = 500,title="Number of samples taken at Month and Year", 
+            x_axis_label='Year - Month', y_axis_label='Number of samples', x_range=sorted_year_month)
+p22 = figure(plot_width = 900, plot_height = 500,title="Number of samples taken at Day of the month", 
+            x_axis_label='Day', y_axis_label='Number of samles')
+p33 = figure(plot_width = 900, plot_height = 500,title="Number of samples taken at Month of the Year", 
+            x_axis_label='Month', y_axis_label='Number of samples')
+p44 = figure(plot_width = 900, plot_height = 500,title="Number of samples taken in each Year 2015-2022", 
+            x_axis_label='Year', y_axis_label='Number of samples')
+
+
+# create empty dictionaries for the bars and lists for the legend items
+bar11, bar22, bar33, bar44 = {}, {}, {}, {}
+items11, items22, items33, items44 = [], [],  [], []
+
+# create the bars for the three plots
+for indx,i in enumerate(list_of_bars):
+    bar11[i] = p11.vbar(x='Year - Month',  top=i, source=cds_month_year, legend_label=i, fill_color=colors[indx], width=0.5)
+    items1.append((dict_legend[i], [bar11[i]]))
+    bar22[i] = p22.vbar(x='Day',  top=i, source=cds_day, legend_label=i, fill_color=colors[indx], width=0.5) 
+    items2.append((dict_legend[i], [bar22[i]]))
+    bar33[i] = p33.vbar(x='Month',  top=i, source=cds_month, legend_label=i, fill_color=colors[indx], width=0.5) 
+    items33.append((dict_legend[i], [bar33[i]]))
+    bar44[i] = p44.vbar(x='Year',  top=i, source=cds_year, legend_label=i, fill_color=colors[indx], width=0.5) 
+    items44.append((dict_legend[i], [bar44[i]]))
+
+p11.xaxis.major_label_orientation = "vertical"
+# customize the legend for the three plots
+customize_legend(p11, items11)
+customize_legend(p22, items22)
+customize_legend(p33, items33)
+customize_legend(p44, items44)
+    
+# set the layout for the plots and add them to three tabs for the three categories
+l11 = layout([[p11]])
+l22 = layout([[p22]])
+l33 = layout([[p33]])
+l44 = layout([[p44]])
+
+tab11 = Panel(child=l11,title="Samples per Month and Year")
+tab22 = Panel(child=l22,title="Samples per Day of Month")
+tab33 = Panel(child=l33,title="Samples per Month of Year")
+tab44 = Panel(child=l44,title="Samples per Year")
+
+tabs_all = Tabs(tabs=[tab11, tab22, tab33, tab44])
+curdoc().add_root(tabs_all)
+
+#display plot
+show(tabs_all)
+
+### Plot the development of the water quality over time for each sample site
 fig_time = px.scatter_mapbox(df, lat="lat" , lon="lon", hover_name="Sample Site", color="Water_quality", opacity=0.5, animation_frame='Year - Month', 
-                             mapbox_style='carto-positron', color_continuous_scale = ["red", "green"], category_orders={'Year - Month':list(np.sort(df['Year - Month'].unique()))}, zoom=8)
+                             mapbox_style='carto-positron', color_continuous_scale = ["red", "green"], category_orders={'Year - Month':sorted_year_month}, zoom=8)
 fig_time.show()
 
 
@@ -195,6 +282,7 @@ def app():
     st.markdown(
       """
       **If you have been or live in NYC, you have probably seen them before! And now you know what they are for!**
+      
       They are spread all over the city to check the water quality in every part of the city and every of the five boroughs
       Below you can explore the exact locations of the sample stations and maybe find to the nearest to where you are living or staying to check it out the next time you walk by!
       """
@@ -204,6 +292,7 @@ def app():
     
     st.markdown('### **Now you have seen how the samples are taken and that the samples are collected manually - Are they really collected regularly?**')
     
+    st.bokeh_chart(tabs_all, use_container_width=True)
     
     st.markdown('### **So we can see that sampels are regularly collected even though it is a manual process  - but what exactly is measured in these samples?**')
     st.markdown(
