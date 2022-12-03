f03ae697a7d35075b3befa256b7103102f32b658
Bufan Deng <43092163+PlevanTem@users.noreply.github.com>
2022-12-04 00:57:55 +0100

    Update results.py

--- pages/results.py
+++ pages/results.py
@@ -16,6 +16,8 @@ from streamlit_folium import folium_static
 import datetime
 from pandas.io.formats.format import Datetime64Formatter
 
+import sklearn
+from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
 import seaborn as sns
 
